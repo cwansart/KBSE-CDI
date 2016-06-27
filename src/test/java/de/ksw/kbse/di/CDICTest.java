9c2c4149979df47c5b157ee64285f21dcd54ab13
Christian Wansart <cwansart@users.noreply.github.com>
2016-06-27 16:38:52 +0200

    Refactored test file

--- src/test/java/de/ksw/kbse/di/CDICTest.java
+++ src/test/java/de/ksw/kbse/di/CDICTest.java
@@ -3,51 +3,52 @@ package de.ksw.kbse.di;
 import de.ksw.kbse.di.mocks.BarImpl;
 import de.ksw.kbse.di.mocks.Foo;
 import static org.junit.Assert.assertTrue;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class CDICTest {
 
     private static CDIC cdic;
+    private Foo foo;
 
     @BeforeClass
     public static void setUpClass() {
         cdic = new CDIC();
     }
+    
+    @Before
+    public void setUp() {
+        foo = cdic.init(Foo.class);
+    }
 
     @Test
     public void testInitsReturnNotNull() {
         System.out.println("Test if init()'s return value is not null");
-        Foo foo = cdic.init(Foo.class);
         assertTrue(foo != null);
     }
 
     @Test
     public void testFoosFieldNotNull() {
         System.out.println("Test if Foo's field baz is not null");
-        Foo foo = cdic.init(Foo.class);
         assertTrue(foo.baz != null);
     }
 
     @Test
     public void testIfBooNotNull() {
         System.out.println("Test if Baz' field boo is not null (recursion test)");
-        Foo foo = cdic.init(Foo.class);
         assertTrue(foo.baz.boo != null);
     }
     
     @Test
     public void testIfIFaceBarNotNull() {
         System.out.println("Test if Baz' interface field bar is not null (recursion test)");
-        Foo foo = cdic.init(Foo.class);
-        System.out.println("foo.bar is: " + foo.bar);
         assertTrue(foo.bar != null);
     }
     
     @Test
     public void testIfIFaceBarIsTypeOfBarImpl() {
         System.out.println("Test if Baz' interface field bar is of type BarImpl");
-        Foo foo = cdic.init(Foo.class);
         assertTrue(foo.bar instanceof BarImpl);
     }
 
